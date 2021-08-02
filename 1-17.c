c1bed691b48ad790d9e6be16b6d834e7c2d74022
Michael <Gamp@Air-Michael.Dlink>
2021-08-02 17:51:50 +0700

    1-17 done

---
+++ 1-17.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define MAXLINE 1000 /* maximum string length */
+
+int getlin(char linef[], int);
+void copy(char to[], char fromf[]);
+
+/* print longest string */
+int  main()
+{
+	int len; /* current length */;
+	int max; /* temporary max length */;
+	char line[MAXLINE]; /*curent string */
+	char longest[MAXLINE]; /* the longest string */
+	max = 0;
+	while ((len=getlin(line, MAXLINE)) > 0)
+		if (len > 80) 
+		{ 
+			copy(longest, line);
+			printf("%s\n", longest);
+		}
+	return(0);
+}
+
+/* getlin: reads a string in s, return length */
+int getlin(char s[], int lim)
+{
+	int c,i;
+	for (i = 0; i < lim -1 && (c = getchar()) != EOF && c != '\n'; i++)
+		s[i] = c;
+	if (c == 'n')
+	{
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;	
+}
+
+/* copy: copying "from" "to"; "to" is big enough */
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
+
